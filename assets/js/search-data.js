d9726077e8f1c3f95c185296a55d70b48e9c7fd6
withersm <103788768+withersm@users.noreply.github.com>
2025-07-14 08:47:18 +0000

    Deploying to gh-pages from @ withersm/withersm.github.io@085c8b93a6e25f3b3cfc9072e90d45934a016c3f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -114,7 +114,7 @@ ninja.data = [{
         title: 'Inspire HEP',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/M.O.Withers.1", "_blank");
+          window.open("https://inspirehep.net/authors/2667784", "_blank");
         },
       },{
         id: 'social-orcid',
@@ -124,25 +124,11 @@ ninja.data = [{
           window.open("https://orcid.org/0000-0001-6012-7730", "_blank");
         },
       },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
-        },
-      },{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://scholar.google.com/citations?user=Ns-vYhIAAAAJ&hl", "_blank");
         },
       },{
       id: 'light-theme',

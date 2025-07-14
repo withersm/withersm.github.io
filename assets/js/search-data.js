1c5918c1b8807410fab4586609c9b735f35bb315
withersm <103788768+withersm@users.noreply.github.com>
2025-07-14 08:36:56 +0000

    Deploying to gh-pages from @ withersm/withersm.github.io@70732a256b1a19a1fb0b3b7aba75560f673c09d5 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -107,14 +107,21 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%77%69%74%68%65%72%73%6D@%73%74%61%6E%66%6F%72%64.%65%64%75", "_blank");
         },
       },{
         id: 'social-inspire',
         title: 'Inspire HEP',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://inspirehep.net/authors/M.O.Withers.1", "_blank");
+        },
+      },{
+        id: 'social-orcid',
+        title: 'ORCID',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://orcid.org/0000-0001-6012-7730", "_blank");
         },
       },{
         id: 'social-rss',
